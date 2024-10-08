be3c2f8f33601133e488723adfa563326dcdd58f
Abel-Ronoh <abellronoh@gmail.com>
2024-10-08 16:52:28 +0300

    array traversals

--- Arrays/arrayTraversal.js
+++ Arrays/arrayTraversal.js
@@ -1,2 +1,19 @@
-let a = 5
-console.log(a)
+// using for loop
+let array = [1,2,3,4,5]
+for (let i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+console.log(array)
+
+//using for of loop
+
+for (let x of array){
+    console.log (x)
+}
+console.log(array)
+
+//using forEach
+
+array.forEach(function(number){
+    console.log(number)
+})
